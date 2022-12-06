4b794fdc416d494c122afb36cc7bc01cfe6315e7
Alexey <redy22@mail.ru>
2022-12-07 01:20:24 +0400

    new

---
+++ Program.cs
@@ -0,0 +1,48 @@
+﻿Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+3, 5 -> 243 (3⁵)
+2, 4 -> 16
+
+Console.WriteLine("Введите число А: ");
+int A = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число B: ");
+int B = Convert.ToInt32(Console.ReadLine());
+int shag = A;
+
+for (int i = 1; i < B; i++)
+{
+    shag = shag * A;
+}
+Console.WriteLine("А в степени В = : " + shag);
+
+
+Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+82 -> 10
+9012 -> 12
+
+Console.WriteLine("Введите число: ");
+int i = Convert.ToInt32(Console.ReadLine());
+int sum = 0;
+
+while (i > 0)
+{
+    int num = i % 10;
+    i = i / 10;
+    sum = sum + num;
+}
+Console.WriteLine("Сумма всех чисел = " + sum);
+
+
+Задача 29: Напишите программу, которая задаёт массив из 8 элементов с клавиатуры и выводит массив на экран.
+
+int[] numbers = new int[8];
+
+for (int i = 0; i < numbers.Length; i++)
+{
+    numbers[i] = new Random().Next(0, 11);
+    Console.Write(" " + Method(i) + " ");
+}
+
+int Method(int a)
+{
+    return numbers[a];
+}
